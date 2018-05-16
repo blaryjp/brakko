df0fb5c4f18435cb364fbce977c9a5a2d5600dfb
blaryjp <blaryjp@users.noreply.github.com>
2018-05-16 22:27:25 +0200

    Update defaultConfig.js

--- conf/defaultConfig.js
+++ conf/defaultConfig.js
@@ -1,18 +1,24 @@
 const conf = require('./commonSettings');
 
 module.exports = {
-    "id": "all",
+    "id": "default",
     "viewports": conf.viewports,
     "scenarios": [],
     "paths": {
         "bitmaps_reference": "backstop_data/bitmaps_reference",
         "bitmaps_test":      "backstop_data/bitmaps_test",
         "casper_scripts":    "casper_scripts",
+        "engine_scripts":    "backstop_data/engine_scripts",
         "html_report":       "backstop_data/html_report",
         "ci_report":         "backstop_data/ci_report"
     },
     "casperFlags": [],
-    "engine": "chrome",
+    "engine": "puppeteer",
+    "engineOptions": {
+        "ignoreHTTPSErrors": true,
+        "args": ["--no-sandbox", "--disable-setuid-sandbox"]
+    },
     "report": ["browser", "CI"],
-    "debug": false
+    "debug": false,
+    "debugWindow": false
 }
